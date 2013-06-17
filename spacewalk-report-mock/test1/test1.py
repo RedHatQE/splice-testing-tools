e22743fa2a044273584e411fae9f392d40575e9b
Vitaly Kuznetsov <vitty@redhat.com>
2013-06-17 18:15:29 +0200

    test1: 2nd system added

--- spacewalk-report-mock/test1/test1.py
+++ spacewalk-report-mock/test1/test1.py
@@ -13,6 +13,10 @@ fd_splice_export = open('%s/template_splice_export.yaml' % path, 'r')
 splice_export = yaml.safe_load(fd_splice_export.read())
 fd_splice_export.close()
 
+fd_splice_export2 = open('%s/template_splice_export2.yaml' % path, 'r')
+splice_export2 = yaml.safe_load(fd_splice_export2.read())
+fd_splice_export2.close()
+
 fd_users = open('%s/template_users.yaml' % path, 'r')
 users = yaml.safe_load(fd_users.read())
 fd_users.close()
@@ -20,12 +24,19 @@ fd_users.close()
 
 def generate(dirname):
     initial_date = datetime.datetime.now() - datetime.timedelta(0, 100 * 4 * 3600)
-    for i in range(1, 101):
+    for i in range(1, 51):
         print_all("%s/step%i" % (dirname, i), {'host_guests': [],
                                                'cloned_channels': [],
                                                'users': [users],
                                                'splice_export': [splice_export]})
         splice_export['last_checkin_time'] = (initial_date + datetime.timedelta(0, i * 4 * 3600)).strftime("%Y-%m-%d %H:%M:%S")
+    for i in range(51, 101):
+        print_all("%s/step%i" % (dirname, i), {'host_guests': [],
+                                               'cloned_channels': [],
+                                               'users': [users],
+                                               'splice_export': [splice_export, splice_export2]})
+        splice_export['last_checkin_time'] = (initial_date + datetime.timedelta(0, i * 4 * 3600)).strftime("%Y-%m-%d %H:%M:%S")
+        splice_export2['last_checkin_time'] = (initial_date + datetime.timedelta(0, i * 4 * 3600)).strftime("%Y-%m-%d %H:%M:%S")
 
 if len(sys.argv) > 1:
     generate(sys.argv[1])
