359988246c07eadf8ad868647ecfb2e39af57ef9
Vitaly Kuznetsov <vitty@redhat.com>
2013-06-19 14:21:09 +0200

    test2

---
+++ splice-tests/test2.py
@@ -0,0 +1,19 @@
+import splicetestlib
+from splicetestlib.splice_testcase import *
+import nose
+
+class test_splice_2(SpliceTestcase, Splice_has_FAKE_SPACEWALK):
+    def _setup(self):
+        splicetestlib.cleanup_katello(self.ss.Instances["KATELLO"][0])
+        splicetestlib.fake_spacewalk_test(self.ss.Instances["FAKE_SPACEWALK"][0], "test2")
+        for step in range(84):
+            splicetestlib.sst_step(self.ss.Instances["FAKE_SPACEWALK"][0])
+
+    def _test(self):
+        pass
+
+    def _cleanup(self):
+        pass
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
