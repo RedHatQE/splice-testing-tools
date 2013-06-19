3100f9cf601d4a646f2bbec83584dfb7fabe2bdf
Vitaly Kuznetsov <vitty@redhat.com>
2013-06-19 12:28:54 +0200

    sat_nosockinfo.py: fix static uuid, typo,..

--- splice-tests/sat_nosockinfo.py
+++ splice-tests/sat_nosockinfo.py
@@ -2,7 +2,7 @@ import splicetestlib
 from splicetestlib.splice_testcase import *
 import nose
 
-class test_splice_bug972942(SpliceTestcase, Splice_has_FAKE_SPACEWALK):
+class test_sat_nosockinfo(SpliceTestcase, Splice_has_FAKE_SPACEWALK):
     def _setup(self):
         splicetestlib.cleanup_katello(self.ss.Instances["KATELLO"][0])
         splicetestlib.fake_spacewalk_test(self.ss.Instances["FAKE_SPACEWALK"][0], "test_sat_nosockinfo")
@@ -10,8 +10,8 @@ class test_splice_bug972942(SpliceTestcase, Splice_has_FAKE_SPACEWALK):
 
     def _test(self):
         uuid = self.katello.list_systems('satellite-1')[0]['uuid']
-        sockets = self.katello.show_system("1e73b493-67bf-4f17-999e-bdcd41b4bf67")['facts']['cpu.cpu_socket(s)']
-        nose.tools.assert_equals(socket, "1")
+        sockets = self.katello.show_system(uuid)['facts']['cpu.cpu_socket(s)']
+        nose.tools.assert_equals(sockets, "1")
 
     def _cleanup(self):
         pass
