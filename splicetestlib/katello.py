ae7b9b15acf0b489e6b4234eb7e3dea847f07b41
Vitaly Kuznetsov <vitty@redhat.com>
2013-06-19 10:43:24 +0200

    show_system api call

--- splicetestlib/katello.py
+++ splicetestlib/katello.py
@@ -33,6 +33,10 @@ class Katello(object):
         """ List available pools """
         return self._request_return(requests.get("https://%s%s/api/systems/%s/pools" % (self.hostname, self.path, system_id), auth=(self.username, self.password), verify=self.verify))
 
+    def show_system(self, system_id):
+        """ List systems """
+        return self._request_return(requests.get('https://%s%s/api/systems/%s' % (self.hostname, self.path, system_id), auth=(self.username, self.password), verify=self.verify))
+
     def delete_subscription(self, system_id, subscription_id):
         """ Detach subscription """
         return self._request_return(requests.delete("https://%s%s/api/systems/%s/subscriptions/%s" % (self.hostname, self.path, system_id, subscription_id), auth=(self.username, self.password), verify=self.verify))
