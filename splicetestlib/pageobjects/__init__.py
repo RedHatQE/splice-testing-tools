2dccb10146569a3381ec79099ea8ff2c2b70c485
dparalen <vetrisko@gmail.com>
2013-07-04 01:48:15 +0200

    introducing webui testing library

---
+++ splicetestlib/pageobjects/__init__.py
@@ -0,0 +1,12 @@
+# Page Objects
+from pageobjects.seleniumwrapper import SeleniumWrapper
+
+SE = SeleniumWrapper()
+
+locators = {
+    "login.username": lambda : SE.find_element_by_name("username"),
+    "login.password": lambda : SE.find_element_by_name("password"),
+    "login.submit": lambda : SE.find_element_by_name("commit"),
+    "login.logout": lambda : SE.find_element_by_link_text("logout")
+}
+
