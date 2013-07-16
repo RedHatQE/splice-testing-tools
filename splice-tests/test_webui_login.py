d313d0fed49050900728f0b3046cada7b3a61cde
dparalen <vetrisko@gmail.com>
2013-07-16 10:50:27 -0400

    introducing login_ctx manager + test case

--- splice-tests/test_webui_login.py
+++ splice-tests/test_webui_login.py
@@ -3,22 +3,34 @@ from splicetestlib.splice_testcase import *
 from splicetestlib.pageobjects import SE
 from splicetestlib.pageobjects.login import LoginPageObject
 from splicetestlib.pageobjects.login import LogoutPageObject
+from splicetestlib.pageobjects.login import login_ctx
 import nose, os, logging, unittest
 
 class test_splice_webui_login(unittest.TestCase, SpliceTestcase, Splice_has_WebUI):
+    sam_user = None
+    sam_password = None
+    sam_url = None
     def _setup(self):
         self.log = logging.getLogger(self.__class__.__name__)
-        SE.reset(url="https://" + self.ss.Instances['SAM'][0].hostname)
+        self.__class__.sam_url = "https://" + self.ss.Instances['SAM'][0].hostname
+        self.__class__.sam_user = self.ss.config["katello_user"] 
+        self.__class__.sam_password = self.ss.config["katello_password"]
+        SE.reset(url=self.sam_url)
 
     def test_01_webui_login(self):
         lpo = LoginPageObject()
-        lpo.username = self.ss.config["katello_user"]
-        lpo.password = self.ss.config["katello_password"]
+        lpo.username = self.sam_user
+        lpo.password = self.sam_password
         lpo.submit()
 
     def test_02_webui_logout(self):
         lpo = LogoutPageObject()
         lpo.submit()
+
+    def test_03_webui_login_ctx(self):
+        with login_ctx(sam_url=self.sam_url, username=self.sam_user, password=self.sam_password):
+            # just check that navigating somewhere works
+            SE.get(SE.current_url + "/sam/dashboard")
 
     def _cleanup(self):
         pass
--- splicetestlib/pageobjects/login.py
+++ splicetestlib/pageobjects/login.py
@@ -6,6 +6,8 @@ from splicetestlib.pageobjects.basepageelement import LinkPageElement
 from splicetestlib.pageobjects.basepageobject import BasePageObject
 from selenium.common.exceptions import NoSuchElementException
 
+from contextlib import contextmanager
+
 class UsernameElement(InputPageElement):
     locator = staticmethod(locators["login.username"])
 
@@ -50,3 +52,17 @@ class LogoutPageObject(BasePageObject):
         self.logout_link.click()
         self.assertIn(locators["login.logout_notice"], SE)
 
+@contextmanager
+def login_ctx(sam_url, username="admin", password="admin"):
+    '''log-in + yield + log-out; SE.current_url restored'''
+    original_url = SE.current_url
+    SE.get(sam_url)
+    login_object = LoginPageObject()
+    login_object.username = username
+    login_object.password = password
+    login_object.submit()
+    SE.get(original_url)
+    yield
+    SE.get(sam_url)
+    logout_object = LogoutPageObject()
+    logout_object.submit()
