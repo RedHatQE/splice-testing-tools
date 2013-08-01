1298c168c25ba5e00026e2044813642122b25f3b
Vitaly Kuznetsov <vitty@redhat.com>
2013-08-01 14:59:12 +0200

    fix testcases for new consolidated report engine

--- splice-tests/test5.py
+++ splice-tests/test5.py
@@ -17,84 +17,126 @@ class test_splice_5(SpliceTestcase, Splice_has_FAKE_SPACEWALK, Splice_has_Manife
         Active report first week
         Expecting 2 current subscriptions
         """
-        self.splice_check_report(days_start=22, days_end=16, current=2)
+        self.splice_check_report(days_start=22, days_end=16, current=2, state=['Active'])
 
     def test_02_inactive_first_week(self):
         """
         Inactive report first week
         Expecting 0 subscriptions
         """
-        self.splice_check_report(days_start=22, days_end=16, inactive=True)
+        self.splice_check_report(days_start=22, days_end=16, state=['Inactive'])
 
-    def test_03_active_second_week(self):
+    def test_03_consolidated_first_week(self):
+        """
+        Consolidated report first week
+        Expecting 2 current subscriptions
+        """
+        self.splice_check_report(days_start=22, days_end=16, current=2, state=['Active', 'Inactive'])
+
+    def test_04_active_second_week(self):
         """
         Active report second week
         Expecting 1 current subscription
         """
-        self.splice_check_report(days_start=13, days_end=8, current=1)
+        self.splice_check_report(days_start=13, days_end=8, current=1, state=['Active'])
 
-    def test_04_inactive_second_week(self):
+    def test_05_inactive_second_week(self):
         """
         Inactive report second week
         Expecting 1 current subscription
         """
-        self.splice_check_report(days_start=13, days_end=8, inactive=True, current=1)
+        self.splice_check_report(days_start=13, days_end=8, state=['Inactive'], current=1)
 
-    def test_05_active_third_week(self):
+    def test_06_consolidated_second_week(self):
+        """
+        Consolidated report second week
+        Expecting 2 current subscription
+        """
+        self.splice_check_report(days_start=13, days_end=8, state=['Active', 'Inactive'], current=2)
+
+    def test_07_active_third_week(self):
         """
         Active report third week
         Expecting 2 current subscriptions
         """
-        self.splice_check_report(days_start=7, days_end=1, current=2)
+        self.splice_check_report(days_start=7, days_end=1, current=2, state=['Active'])
 
-    def test_06_inactive_third_week(self):
+    def test_08_inactive_third_week(self):
         """
         Inactive report third week
         Expecting 0 subscriptions
         """
-        self.splice_check_report(days_start=7, days_end=1, inactive=True)
+        self.splice_check_report(days_start=7, days_end=1, state=['Inactive'])
 
-    def test_07_active_all_weeks(self):
+    def test_09_consolidated_third_week(self):
+        """
+        Consolidated report third week
+        Expecting 2 current subscriptions
+        """
+        self.splice_check_report(days_start=7, days_end=1, current=2, state=['Active', 'Inactive'])
+
+    def test_10_active_all_weeks(self):
         """
         Active report all weeks
         Expecting 2 current subscriptions
         """
-        self.splice_check_report(days_start=22, days_end=1, current=2)
+        self.splice_check_report(days_start=22, days_end=1, current=2, state=['Active'])
 
-    def test_08_inactive_all_week(self):
+    def test_11_inactive_all_week(self):
         """
         Inactive report all weeks
         Expecting 0 subscriptions
         """
-        self.splice_check_report(days_start=22, days_end=1, inactive=True)
+        self.splice_check_report(days_start=22, days_end=1, state=['Inactive'])
 
-    def test_09_active_history(self):
+    def test_12_consolidated_all_weeks(self):
+        """
+        Consolidated report all weeks
+        Expecting 2 current subscriptions
+        """
+        self.splice_check_report(days_start=22, days_end=1, current=2, state=['Active', 'Inactive'])
+
+    def test_13_active_history(self):
         """
         Active report month ago
         Expecting 0 subscriptions
         """
-        self.splice_check_report(days_start=51, days_end=41)
+        self.splice_check_report(days_start=51, days_end=41, state=['Active'])
 
-    def test_10_inactive_history(self):
+    def test_14_inactive_history(self):
         """
         Inactive report month ago
         Expecting 0 subscriptions
         """
-        self.splice_check_report(days_start=51, days_end=41, inactive=True)
+        self.splice_check_report(days_start=51, days_end=41, state=['Inactive'])
 
-    def test_11_active_future(self):
+    def test_15_consolidated_history(self):
+        """
+        Consolidated report month ago
+        Expecting 0 subscriptions
+        """
+        self.splice_check_report(days_start=51, days_end=41, state=['Active', 'Inactive'])
+
+    def test_16_active_future(self):
         """
         Active report future month
         Expecting 0 subscriptions
         """
-        self.splice_check_report(days_start=-21, days_end=-31)
+        self.splice_check_report(days_start=-21, days_end=-31, state=['Active'])
 
-    def test_12_inactive_future(self):
+    def test_17_inactive_future(self):
         """
         Inactive report future month
         Expecting 2 current subscriptions
         """
-        self.splice_check_report(days_start=-21, days_end=-31, inactive=True, current=2)
+        self.splice_check_report(days_start=-21, days_end=-31, state=['Inactive'], current=2)
+
+    def test_18_consolidated_future(self):
+        """
+        Consolidated report future month
+        Expecting 2 current subscriptions
+        """
+        self.splice_check_report(days_start=-21, days_end=-31, state=['Active', 'Inactive'], current=2)
 
     def _cleanup(self):
         splicetestlib.cleanup_katello(self.ss.Instances["KATELLO"][0], self.katello.password)
