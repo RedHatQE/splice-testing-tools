1b86edfe1755f3f91e27e2ef054237251b3d79d9
Vitaly Kuznetsov <vitty@redhat.com>
2013-08-20 10:46:42 +0200

    test_deleted_1.py: 4 days

--- spacewalk-report-mock/test_deleted_1/test_deleted_1.py
+++ spacewalk-report-mock/test_deleted_1/test_deleted_1.py
@@ -19,20 +19,20 @@ fd_users.close()
 
 
 def generate(dirname):
-    initial_date = datetime.datetime.now() - datetime.timedelta(0, 84 * 4 * 3600)
+    initial_date = datetime.datetime.now() - datetime.timedelta(0, 24 * 4 * 3600)
     print_all("%s/step1" % dirname, {'host_guests': [],
                                      'cloned_channels': [],
                                      'users': [users],
                                      'splice_export': [],
                                      'fake_checkin_date': initial_date.strftime("%Y-%m-%d %H:%M:%S")})
-    for i in range(1, 42):
+    for i in range(1, 13):
         splice_export['last_checkin_time'] = (initial_date + datetime.timedelta(0, i * 4 * 3600)).strftime("%Y-%m-%d %H:%M:%S")
         print_all("%s/step%i" % (dirname, i + 1), {'host_guests': [],
                                                    'cloned_channels': [],
                                                    'users': [users],
                                                    'splice_export': [splice_export],
                                                    'fake_checkin_date': splice_export['last_checkin_time']})
-    for i in range(42, 85):
+    for i in range(13, 25):
         splice_export['last_checkin_time'] = (initial_date + datetime.timedelta(0, i * 4 * 3600)).strftime("%Y-%m-%d %H:%M:%S")
         print_all("%s/step%i" % (dirname, i + 1), {'host_guests': [],
                                                    'cloned_channels': [],
