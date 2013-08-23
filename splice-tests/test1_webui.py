d72d6f4bf01950feb9749e260e30f180b1e59a68
dparalen <vetrisko@gmail.com>
2013-08-23 10:06:10 -0400

    introducing set1--webui tests

---
+++ splice-tests/test1_webui.py
@@ -0,0 +1,29 @@
+import splicetestlib
+from splicetestlib.splice_testcase import *
+import datetime
+import nose
+import pageobjects
+from pageobjects import login
+from selenium_wrapper import SE
+
+class test_splice_1_webui(SpliceTestcase, Splice_has_FAKE_SPACEWALK, Splice_has_Manifest, Splice_has_WebUI):
+    def _setup(self):
+        splicetestlib.fake_spacewalk_env(self.ss.Instances["FAKE_SPACEWALK"][0], "test1")
+        splicetestlib.sst_step(self.ss.Instances["KATELLO"][0], self.ss.Instances["FAKE_SPACEWALK"][0])
+        # uploading manifest
+        self.katello.upload_manifest("satellite-1", self.ss.config["manifest"])
+        for step in range(36):
+       	    splicetestlib.sst_step(self.ss.Instances["KATELLO"][0], self.ss.Instances["FAKE_SPACEWALK"][0])
+
+    def test_01_active_week(self):
+        """
+        Active report last week
+        Expecting 1 current and 1 invalid subscription
+        """
+        Splice_has_WebUI.splice_check_report(days_start=7, days_end=1, current=1, invalid=1, state=['Active'])
+
+    def _cleanup(self):
+        splicetestlib.cleanup_katello(self.ss.Instances["KATELLO"][0], self.katello)
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
