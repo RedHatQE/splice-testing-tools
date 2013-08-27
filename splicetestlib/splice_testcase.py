7532eac3e3a4194f78140d605c3d24370b91a14b
dparalen <vetrisko@gmail.com>
2013-08-27 10:14:08 -0400

    fix: getattr does not work; implemented common environment context

--- splicetestlib/splice_testcase.py
+++ splicetestlib/splice_testcase.py
@@ -13,6 +13,7 @@ from pageobjects.users import user_experimental_ui_ctx
 from pageobjects.sampageobject import organisation_ctx
 from pageobjects.filters import filter_date_range_ctx, filter_hours_ctx
 from selenium_wrapper import SE, current_url
+from contextlib import contextmanager
 
 def traverse_mro(type_instance):
     # traverse mro of a type_instance returning a list of type_instances
@@ -216,6 +217,16 @@ class Splice_has_WebUI(object):
             'url': "https://" + ss.Instances['KATELLO'][0].parameters['public_dns_name']
         })
 
+    @staticmethod
+    @contextmanager
+    def _env_ctx(url, username, password, organization):
+         with current_url(url):
+            with login_ctx(username, password):
+                with user_experimental_ui_ctx(username):
+                    with organisation_ctx(organization):
+                        yield
+
+
     @classmethod
     def splice_check_report(typeinstance, days_start=None, days_end=None, past_hours=None, state=[u'Active', u'Inactive', u'Deleted'], current=0, invalid=0, insufficient=0, organizations=[u'Testing Org']):
         report_page = None
@@ -225,41 +236,35 @@ class Splice_has_WebUI(object):
             # date-range mode
             start_date, end_date = report.date_ago(days_start), report.date_ago(days_end)
             filter_name = report.dates_filter_name(start_date, end_date, state)
-            with current_url(KATELLO.url):
-                with login_ctx(KATELLO.user, KATELLO.password):
-                    with user_experimental_ui_ctx(KATELLO.user):
-                        with organisation_ctx(organizations[0]):
-                            with filter_date_range_ctx(
-                                    name=filter_name,
-                                    start_date=start_date,
-                                    end_date=end_date,
-                                    organizations=organizations,
-                                    lifecycle_states=state
-                                ) as (filters_page, report_filter):
-                                report_page = getattr(filters_page, filter_name).run_report()
-                                # assert the values
-                                nose.tools.assert_equal(report_page.current_subscriptions.count.text, unicode(current))
-                                nose.tools.assert_equal(report_page.insufficient_subscriptions.count.text, unicode(insufficient))
-                                nose.tools.assert_equal(report_page.invalid_subscriptions.count.text, unicode(invalid))
+            with typeinstance._env_ctx(KATELLO.url, KATELLO.user, KATELLO.password, organizations[0]):
+                with filter_date_range_ctx(
+                        name=filter_name,
+                        start_date=start_date,
+                        end_date=end_date,
+                        organizations=organizations,
+                        lifecycle_states=state
+                    ) as (filters_page, report_filter):
+                    report_page = report_filter.run_report()
+                    # assert the values
+                    nose.tools.assert_equal(report_page.current_subscriptions.count.text, unicode(current))
+                    nose.tools.assert_equal(report_page.insufficient_subscriptions.count.text, unicode(insufficient))
+                    nose.tools.assert_equal(report_page.invalid_subscriptions.count.text, unicode(invalid))
                                 
         elif past_hours is not None:
             # hours-based operation
-            filter_name = reports.hours_filter_name(past_hours, state)
-            with current_url(KATELLO.url):
-                with login_ctx(KATELLO.user, KATELLO.password):
-                    with user_experimental_ui_ctx(KATELLO.user):
-                        with organisation_ctx(organizations[0]):
-                            with filter_hours_ctx(
-                                    name=filter_name,
-                                    hours=past_hours,
-                                    organizations=organizations,
-                                    lifecycle_states=state
-                                ) as (filters_page, report_filter):
-                                report_page = getattr(filters_page, filter_name).run_report()
-                                # assert the values
-                                nose.tools.assert_equal(report_page.current_subscriptions.count.text, unicode(current))
-                                nose.tools.assert_equal(report_page.insufficient_subscriptions.count.text, unicode(insufficient))
-                                nose.tools.assert_equal(report_page.invalid_subscriptions.count.text, unicode(invalid))
+            filter_name = report.hours_filter_name(past_hours, state)
+            with typeinstance._env_ctx(KATELLO.url, KATELLO.user, KATELLO.password, organizations[0]):
+                with filter_hours_ctx(
+                        name=filter_name,
+                        hours=past_hours,
+                        organizations=organizations,
+                        lifecycle_states=state
+                    ) as (filters_page, report_filter):
+                    report_page = report_filter.run_report()
+                    # assert the values
+                    nose.tools.assert_equal(report_page.current_subscriptions.count.text, unicode(current))
+                    nose.tools.assert_equal(report_page.insufficient_subscriptions.count.text, unicode(insufficient))
+                    nose.tools.assert_equal(report_page.invalid_subscriptions.count.text, unicode(invalid))
         else:
             # Wrong usage
             assert False, "Wrong usage"
