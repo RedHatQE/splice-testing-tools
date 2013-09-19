9a9298c34584e6cb7f230b9b0c567f78cec3a846
Vitaly Kuznetsov <vitty@redhat.com>
2013-09-19 13:13:50 +0200

    test_rhel4_arches (improved test_rhel4_em64t) implemented

--- spacewalk-report-mock/test_rhel4_arches/test_rhel4_arches.py
+++ spacewalk-report-mock/test_rhel4_arches/test_rhel4_arches.py
@@ -24,13 +24,25 @@ def generate(dirname):
                                      'users': [users],
                                      'splice_export': []})
     initial_date = datetime.datetime.now() - datetime.timedelta(0, 4 * 3600)
+    splice_export['name'] = 'host1'
+    splice_export['hostname'] = 'host1'
     splice_export['last_checkin_time'] = initial_date.strftime("%Y-%m-%d %H:%M:%S")
     splice_export['software_channel']='rhel-x86_64-as-4'
     splice_export['architecture']='EM64T'
+    splice_export_list = []
+    splice_export_list.append(splice_export.copy())
+
+    for server_id, name, arch in [('1000020000', 'host2', 'ppc64pseries'), ('1000030000', 'host3', 'ppc64iseries'), ('1000040000', 'host4', 'athlon')]:
+        splice_export['name'] = name
+        splice_export['hostname'] = name
+        splice_export['architecture'] = arch
+        splice_export['server_id'] = server_id
+        splice_export_list.append(splice_export.copy())
+
     print_all("%s/step2" % dirname, {'host_guests': [],
                                      'cloned_channels': [],
                                      'users': [users],
-                                     'splice_export': [splice_export]})
+                                     'splice_export': splice_export_list})
 
 if len(sys.argv) > 1:
     generate(sys.argv[1])
---
+++ splice-tests/test_rhel4_arches.py
@@ -0,0 +1,37 @@
+import splicetestlib
+from splicetestlib.splice_testcase import *
+import nose
+
+class test_rhel4_arches(SpliceTestcase, Splice_has_FAKE_SPACEWALK, Splice_has_Manifest):
+    def _setup(self):
+        splicetestlib.fake_spacewalk_env(self.ss.Instances["FAKE_SPACEWALK"][0], "test_rhel4_arches")
+        splicetestlib.sst_step(self.ss.Instances["KATELLO"][0], self.ss.Instances["FAKE_SPACEWALK"][0])
+        # uploading manifest
+        self.katello.upload_manifest("satellite-1", self.ss.config["manifest"])
+        splicetestlib.sst_step(self.ss.Instances["KATELLO"][0], self.ss.Instances["FAKE_SPACEWALK"][0])
+
+    def test_01_products_and_arches(self):
+        """
+        Checking arches and RHEL product
+        """
+        systems = self.katello.list_systems('satellite-1')
+        assert len(systems) == 4, 'Expecting 4 systems in setup'
+
+        for sys in systems:
+            info = self.katello.show_system(sys['uuid'])
+            nose.tools.assert_equals(info['installedProducts'][0]['productId'], '69')
+            if sys['name'] == 'host1':
+                nose.tools.assert_equals(info['facts']['uname.machine'], 'x86_64')
+                nose.tools.assert_equals(info['facts']['lscpu.architecture'], 'x86_64')
+            elif sys['name'] == 'host4':
+                nose.tools.assert_equals(info['facts']['uname.machine'], 'i386')
+                nose.tools.assert_equals(info['facts']['lscpu.architecture'], 'i386')
+            else:
+                nose.tools.assert_equals(info['facts']['uname.machine'], 'ppc64')
+                nose.tools.assert_equals(info['facts']['lscpu.architecture'], 'ppc64')
+
+    def _cleanup(self):
+        splicetestlib.cleanup_katello(self.ss.Instances["KATELLO"][0], self.katello, full_reset=True)
+
+if __name__ == "__main__":
+    nose.run(defaultTest=__name__, argv=[__file__, '-v'])
