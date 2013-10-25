d75a1218da4324d6b5ef9b673ae64b2472e5edfb
Vitaly Kuznetsov <vitty@redhat.com>
2013-10-25 11:36:29 +0200

    test_distributor test data (2 orgs)

---
+++ spacewalk-report-mock/test_distributor/test_distributor.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import datetime
+import yaml
+
+path = os.path.dirname(sys.argv[0])
+
+sys.path.append(path + "/../")
+
+from common import *
+
+fd_splice_export = open('%s/template_splice_export.yaml' % path, 'r')
+splice_export = yaml.safe_load(fd_splice_export.read())
+fd_splice_export.close()
+
+fd_users = open('%s/template_users.yaml' % path, 'r')
+users = yaml.safe_load(fd_users.read())
+fd_users.close()
+
+
+def generate(dirname):
+    splice_export[1]['org_id'] = '2'
+    splice_export[1]['registered_by'] = 'adminorg2'
+    splice_export[1]['organization'] = 'Testing Org2'
+    print_all("%s/step1" % dirname, {'host_guests': [],
+                                     'cloned_channels': [],
+                                     'users': users,
+                                     'splice_export': splice_export})
+
+if len(sys.argv) > 1:
+    generate(sys.argv[1])
+else:
+    generate("./")
